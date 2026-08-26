3e1f3eb5b371514293e7fd7a7df253a8e81d0f1f
lowkeyloading <emilyanthonybriggs@gmail.com>
2026-08-25 17:15:31 -0700

    Fix Firebase Admin credentials for Vercel

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,13 +1,9 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import fs from "fs";
 
 const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    process.env.FIREBASE_ADMIN_KEY_PATH!,
-    "utf8"
-  )
+  process.env.FIREBASE_ADMIN_KEY!
 );
 
 const adminApp =
